18f68219b1cb9f12bf8c501cedc1a5ebeb6a72d3
YongLiu <YongLiu@Yongs-MacBook-Pro.local>
2015-11-14 18:40:27 -0500

    file in place

---
+++ src/os.java
@@ -0,0 +1,29 @@
+
+import java.util.List;
+
+public class os {
+	
+	public static void startup(){
+		sos.ontrace();
+	}
+	
+	public static void Crint(int [] a, int [] p){
+		
+	}
+	
+	public static void Dskint(int [] a, int [] p){
+		
+	}
+	
+	public static void Drmint(int [] a, int [] p){
+		
+	}
+	
+	public static void Svc(int [] a, int [] p){
+		
+	}
+	
+	public static void Tro(int [] a, int [] p){
+	}
+}
+
